49cd61b908cf0aaa7783650c42b916cd47708fc8
gorkaerana <gorka.e.r@gmail.com>
2021-12-06 00:13:01 +0100

    2021/day04.jl: solved parts 1 and 2.

--- 2021/day04.jl
+++ 2021/day04.jl
@@ -4,48 +4,46 @@ input = @pipe dirname(@__FILE__) |>
     joinpath(_, "input", "day04.txt") |>
     readlines(_)
 draws = parse.(Int, split(input[1], ","))
-cards = zeros(100, 5, 5)
+cards = zeros(5, 5, 100)
 for (i, line_no) in enumerate(3:6:length(input))
-    card = split.(input[line_no:line_no+4], r"\s+")
+    card = @pipe input[line_no:line_no+4] |>
+        strip.(_) |>
+        split.(_, r"\s+")
     for (j, row) in enumerate(card)
-        # "15  9" splits into "15", "", "9"
-        numbers = filter(r -> r != "", row)
-        cards[i, j, :] = parse.(Int, numbers)
+        cards[j, :, i] = parse.(Int, row)
     end
 end
 
-function is_bingo_card(arr, dims)
-    sums = sum(arr, dims=dims)
-    return sums, sum(sums .== 5) != 0
+function card_has_bingo(mask)
+    for dims in 1:2
+        if any(sum(mask, dims=dims) .== 5)
+            return true
+        end
+    end
+    return false
 end
 
-function winning_card_no(sums)
-    argmax(sums)[1]
+function card_score(card, mask, draw)
+    sum(card .* (.!mask)) * draw
 end
 
-function card_score(cards, matches, sums, draw)
-    winning_card_idx = winning_card_no(sums)
-    winning_card = cards[winning_card_idx, :, :]
-    unmatched_mask = .!Bool.(matches[winning_card_idx, :, :])
-    return sum(winning_card[unmatched_mask]) * draw
-end
-
-matches = zeros(100, 5, 5)
+marked_mask = Bool.(zeros(5, 5, 100))
+bingo_mask = Bool.(zeros(100))
+first_bingo = true
+score = 0
 for (idx, draw) in enumerate(draws)
-    global matches
-    matches = matches + (cards .== draw)
-    vertical_sums, vertical_match = is_bingo_card(matches, 2)
-    horizontal_sums, horizontal_match = is_bingo_card(matches, 3)
-    # Part 1
-    if vertical_match
-        score = card_score(cards, matches, vertical_sums, draw)
-        @show score
-        break
+    global first_bingo, score
+    marked_mask[:, :, :] += (cards .== draw)
+    bingo_cards = card_has_bingo.(eachslice(marked_mask, dims=3))
+    if sum(bingo_cards) > sum(bingo_mask)
+        latest_bingo = bingo_mask[:] .⊻ bingo_cards[:]
+        score = @pipe latest_bingo |>
+            card_score(cards[:, :, _], marked_mask[:, :, _], draw)
+        if first_bingo
+            println("Part 1: the score of the first winning board is $(score).")
+            first_bingo = false
+        end
+        bingo_mask[:] = bingo_mask[:] .| bingo_cards[:]
     end
-    if horizontal_match
-        score = card_score(cards, matches, horizontal_sums, draw)
-        @show score
-        break
-    end
-    # Part 2
 end
+println("Part 2: the score of the last winning board is $(score).")
