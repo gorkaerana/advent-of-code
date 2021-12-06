a35aa5bc0a66f2cac991415af33b61920e4a4c65
Gorka Eraña <eranagorka@gmail.com>
2021-12-06 13:50:08 +0100

    2021/day06.jl: solved part 2

--- 2021/day06.jl
+++ 2021/day06.jl
@@ -1,23 +1,34 @@
 using Pipe: @pipe
+using DataStructures: counter, DefaultDict
 
 lanternfish = @pipe dirname(@__FILE__) |>
     joinpath(_, "input", "day06.txt") |>
     readlines(_)[1] |>
     split(_, ",") |>
-    parse.(Int, _)
-lanternfish = [3,4,3,1,2]
+    parse.(Int, _) |>
+    counter(_)
 
-function epoch(population_array)
-    population_array .-= 1
-    for _ in 1:sum(population_array .== -1)
-        push!(population_array, 8)
+function epoch(population_counter)
+    updated_population = DefaultDict(0)
+    for (days_to_labour, n) in population_counter
+        updated_population[days_to_labour-1] += n
     end
-    population_array[population_array .== -1] .= 6
-    return population_array
+    if -1 in keys(updated_population)
+        n_gave_birth = pop!(updated_population, -1)
+        # Set counter to 8 for baby lanternfish
+        updated_population[8] += n_gave_birth
+        # Reset days to 6 for lanternfish that just gave birth
+        updated_population[6] += n_gave_birth
+    end
+    return updated_population
 end
 
-n_days = 80
+n_days = 256
 for day_no in 1:n_days
-    lanternfish[:] = epoch(lanternfish)
+    global lanternfish
+    lanternfish = epoch(lanternfish)
+    if day_no == 80
+        println("Part 1: number of lanternfish after $(day_no) days: $(sum(values(lanternfish))).")
+    end
 end
-println("Part 1: number of lanternfish after $(n_days) days: $(length(lanternfish)).")
+println("Part 2: number of lanternfish after $(n_days) days: $(sum(values(lanternfish))).")
