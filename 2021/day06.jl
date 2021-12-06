58de2ce5f7eb806b4e60eb548b5b15feff56a6b0
Gorka Eraña <eranagorka@gmail.com>
2021-12-06 14:00:58 +0100

    2021/day06.jl: misplaced file in previous commit

--- 2021/day06.jl
+++ 2021/day06.jl
@@ -1,13 +1,6 @@
 using Pipe: @pipe
 using DataStructures: counter, DefaultDict
 
-lanternfish = @pipe dirname(@__FILE__) |>
-    joinpath(_, "input", "day06.txt") |>
-    readlines(_)[1] |>
-    split(_, ",") |>
-    parse.(Int, _) |>
-    counter(_)
-
 function epoch(population_counter)
     updated_population = DefaultDict(0)
     for (days_to_labour, n) in population_counter
@@ -23,12 +16,25 @@ function epoch(population_counter)
     return updated_population
 end
 
-n_days = 256
-for day_no in 1:n_days
-    global lanternfish
-    lanternfish = epoch(lanternfish)
-    if day_no == 80
-        println("Part 1: number of lanternfish after $(day_no) days: $(sum(values(lanternfish))).")
+let
+    lanternfish = @pipe dirname(@__FILE__) |>
+        joinpath(_, "input", "day06.txt") |>
+        readlines(_)[1] |>
+        split(_, ",") |>
+        parse.(Int, _) |>
+        counter(_)
+    n_days = 256
+    for day_no in 1:n_days
+        lanternfish = epoch(lanternfish)
+        if day_no == 80
+            println(
+                "Part 1: number of lanternfish after $(day_no) days: " *
+                    "$(sum(values(lanternfish)))."
+            )
+        end
     end
+    println(
+        "Part 2: number of lanternfish after $(n_days) days: " *
+            "$(sum(values(lanternfish)))."
+    )
 end
-println("Part 2: number of lanternfish after $(n_days) days: $(sum(values(lanternfish))).")
