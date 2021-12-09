c0755dd8b2122d54db294c0d41e959ec531f65e0
gorkaerana <gorka.e.r@gmail.com>
2021-12-09 22:28:07 +0100

    2021/day08.jl: refactoring towards solving part 2

--- 2021/day08.jl
+++ 2021/day08.jl
@@ -1,25 +1,50 @@
+using DataStructures: counter
 using Pipe: @pipe
 
-input = @pipe joinpath(@__DIR__, "input", "day08.txt") |>
-    readlines(_) |>
-    split.(_, " | ") |>
-    map(x -> x[2], _) |>
-    split.(_, " ") |>
-    map(l -> length.(l), _) |>
-    vcat(_...) |>
-    filter(x -> x in [2 4 3 7], _) |>
-    length(_)
-
-n_segments = Dict(
-    0 => 6,
-    1 => 2, #
-    2 => 5,
-    3 => 5,
-    4 => 4, #
-    5 => 5,
-    6 => 6,
-    7 => 3, #
-    8 => 7, #
-    9 => 6
+digit_segments = Dict(
+    0 => "abcefg",
+    1 => "cf",
+    2 => "acdeg",
+    3 => "acdfg",
+    4 => "bcdf",
+    5 => "abdfg",
+    6 => "abdefg",
+    7 => "acf",
+    8 => "abcdefg",
+    9 => "abcdfg"
 )
-@show input
+
+non_repeated_n_segments = @pipe digit_segments |>
+    values(_) |>
+    length.(_) |>
+    counter(_) |>
+    filter(p -> last(p) == 1, _) |>
+    keys(_)
+
+function process_entry(string)
+    signal_patterns, output_value = @pipe string |>
+        split(_, " | ") |>
+        split.(_, " ")
+    return signal_patterns, output_value
+end
+
+input = @pipe joinpath(@__DIR__, "input", "day08.txt") |>
+    readlines(_)
+
+# part1 =  @pipe input |>
+#     process_entry.(_) |>
+#     map(entry -> getindex(entry, 2), _) |>
+#     vcat(_...) |>
+#     length.(_) |>
+#     filter(x -> x in non_repeated_n_segments, _) |>
+#     length(_)
+# @show part1
+
+# part2 = @pipe input |>
+#     split.(_, " | ") |>
+#     map(x -> getindex(x, 1), _) |>
+#     split.(_, " ") |>
+#     vcat(_...) |>
+#     length.(_) |>
+#     counter(_)
+# @show part2
